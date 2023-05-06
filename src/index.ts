efd57f2f3865b0c2bd2392edd82e3fae4af7ebde
rozhkovs <rozhkovse1@gmail.com>
2023-05-06 20:38:57 +0700

    refactor: remove unnecessary constant declarations

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export const EMPTY_OBJECT = Object.freeze({});
 export const EMPTY_ARRAY = Object.freeze([]);
 export const EMPTY_CALLBACK = Object.freeze(() => {});
 
-export const E_OBJ = EMPTY_OBJECT;
-export const E_ARR = EMPTY_ARRAY;
-export const E_CB = EMPTY_CALLBACK;
+export {
+  EMPTY_OBJECT as E_OBJ,
+  EMPTY_ARRAY as E_ARR,
+  EMPTY_CALLBACK as E_CB,
+}
