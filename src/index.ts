3f7f9cdd7cfcfc2881700a2160daa302f80e456d
rozhkovs <rozhkovse1@gmail.com>
2023-05-06 20:41:43 +0700

    refactor: fix lint

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ export const EMPTY_OBJECT = Object.freeze({});
 export const EMPTY_ARRAY = Object.freeze([]);
 export const EMPTY_CALLBACK = Object.freeze(() => {});
 
-export {
-  EMPTY_OBJECT as E_OBJ,
-  EMPTY_ARRAY as E_ARR,
-  EMPTY_CALLBACK as E_CB,
-}
+export {EMPTY_OBJECT as E_OBJ, EMPTY_ARRAY as E_ARR, EMPTY_CALLBACK as E_CB};
